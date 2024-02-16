e8c66af2d08e36bf1b8e1923c6411333e4dac714
Vincent Rubinetti <vince.rubinetti@gmail.com>
2024-02-15 22:11:40 -0500

    basic scaffold

---
+++ app/src/App.tsx
@@ -0,0 +1,79 @@
+import Footer from "@/sections/Footer";
+import Header from "@/sections/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc sed
+          velit dignissim sodales ut eu sem integer. Fermentum odio eu feugiat
+          pretium nibh ipsum consequat. Etiam erat velit scelerisque in dictum
+          non consectetur. Sed pulvinar proin gravida hendrerit. Vitae turpis
+          massa sed elementum. Sagittis eu volutpat odio facilisis mauris sit.
+          Suscipit tellus mauris a diam maecenas sed. Accumsan sit amet nulla
+          facilisi morbi. Integer quis auctor elit sed vulputate mi. Luctus
+          venenatis lectus magna fringilla urna. Sit amet purus gravida quis
+          blandit turpis cursus. Vel orci porta non pulvinar. Quis imperdiet
+          massa tincidunt nunc pulvinar sapien et. Aliquet bibendum enim
+          facilisis gravida neque. Curabitur vitae nunc sed velit.
+        </p>
+        <p>
+          Blandit turpis cursus in hac habitasse platea dictumst quisque
+          sagittis. Vel facilisis volutpat est velit. Vel risus commodo viverra
+          maecenas accumsan lacus vel facilisis volutpat. Sit amet tellus cras
+          adipiscing enim eu turpis egestas. Aliquam etiam erat velit
+          scelerisque in dictum. Dignissim convallis aenean et tortor at risus
+          viverra adipiscing. Quisque id diam vel quam elementum pulvinar etiam
+          non quam. Sit amet mattis vulputate enim nulla aliquet porttitor
+          lacus. Vulputate eu scelerisque felis imperdiet proin fermentum.
+          Pulvinar etiam non quam lacus suspendisse faucibus interdum posuere
+          lorem. Nisl pretium fusce id velit ut tortor pretium viverra. A
+          iaculis at erat pellentesque adipiscing commodo elit. Senectus et
+          netus et malesuada. Maecenas sed enim ut sem. Massa massa ultricies mi
+          quis hendrerit dolor magna eget est. Non consectetur a erat nam at
+          lectus urna.
+        </p>
+        <p>
+          Dolor morbi non arcu risus quis varius quam quisque id. At volutpat
+          diam ut venenatis tellus in metus. Non blandit massa enim nec dui.
+          Scelerisque varius morbi enim nunc. Nisi porta lorem mollis aliquam ut
+          porttitor. Leo urna molestie at elementum eu facilisis sed odio morbi.
+          Euismod nisi porta lorem mollis aliquam ut porttitor. A erat nam at
+          lectus urna duis convallis convallis. Integer malesuada nunc vel risus
+          commodo viverra maecenas accumsan lacus. Ac auctor augue mauris augue
+          neque gravida in fermentum et. Commodo sed egestas egestas fringilla.
+        </p>
+        <p>
+          Sit amet purus gravida quis. Tincidunt lobortis feugiat vivamus at
+          augue eget arcu dictum varius. Morbi blandit cursus risus at ultrices
+          mi. Facilisi morbi tempus iaculis urna id volutpat lacus. Fringilla
+          est ullamcorper eget nulla facilisi. Cras tincidunt lobortis feugiat
+          vivamus at augue. Elit sed vulputate mi sit amet. Sodales neque
+          sodales ut etiam sit amet nisl. Odio euismod lacinia at quis risus.
+          Ipsum faucibus vitae aliquet nec ullamcorper. Ultricies integer quis
+          auctor elit sed vulputate. Sed enim ut sem viverra. Sit amet risus
+          nullam eget felis eget. Mollis nunc sed id semper risus in hendrerit
+          gravida rutrum. Lectus proin nibh nisl condimentum id. Nunc id cursus
+          metus aliquam eleifend mi in nulla.
+        </p>
+        <p>
+          Augue mauris augue neque gravida in fermentum et sollicitudin ac.
+          Auctor eu augue ut lectus arcu bibendum at varius vel. Consectetur
+          adipiscing elit pellentesque habitant morbi. Metus vulputate eu
+          scelerisque felis imperdiet proin fermentum leo vel. Volutpat lacus
+          laoreet non curabitur gravida arcu ac tortor. Nibh tellus molestie
+          nunc non blandit massa. Lacus viverra vitae congue eu consequat ac
+          felis. Lectus proin nibh nisl condimentum id venenatis a condimentum.
+          Est sit amet facilisis magna. Ipsum consequat nisl vel pretium lectus
+          quam.
+        </p>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ app/src/sections/Footer.tsx
@@ -0,0 +1,12 @@
+import classes from "./Footer.module.css";
+
+function Footer() {
+  return (
+    <footer>
+      <div className={classes.summary}>5 edit(s)</div>
+      <button>Submit</button>
+    </footer>
+  );
+}
+
+export default Footer;
