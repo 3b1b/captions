e91a2231c4458079b4b196c61a8e2b6d451dee32
Grant Sanderson <grant@3blue1brown.com>
2024-02-21 14:44:16 -0800

    Change base url

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/captions",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: { "@": fileURLToPath(new URL("./src", import.meta.url)) },
